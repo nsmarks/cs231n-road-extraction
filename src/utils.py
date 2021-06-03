9033b91975377593f8b6274e594d90b8ab23520f
Nathan Marks <nsmarks@stanford.edu>
2021-06-03 16:30:22 -0700

    get example images

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,8 @@ import torch.nn as nn
 # import numpy as np
 # from PIL import Image
 import matplotlib.pyplot as plt
+import collections
+import sys
 
 
 def saveOutputAsImage(output, path):
@@ -22,11 +24,47 @@ if (__name__ == '__main__'):
     # import model
     model_weights = torch.load('../save/Baseline/baseline1/model_statedict.pt')
     # print ('model: ', model)
-    model = models.segmentation.fcn_resnet101(pretrained=True, progress=True)
-    model.classifier[4] = nn.Conv2d(512, 2, kernel_size=(1,1), stride=(1,1))
-    model.aux_classifier[4] = nn.Conv2d(256, 2, kernel_size=(1,1), stride=(1,1))
-    model.load_state_dict(model_weights)
-    model.eval()
+    baseline = models.segmentation.fcn_resnet101(pretrained=True, progress=True)
+    baseline.classifier[4] = nn.Conv2d(512, 2, kernel_size=(1,1), stride=(1,1))
+    baseline.aux_classifier[4] = nn.Conv2d(256, 2, kernel_size=(1,1), stride=(1,1))
+    baseline.load_state_dict(model_weights)
+    baseline.eval()
+
+
+
+    # adversarial:
+    # import model
+    model_weights = torch.load('../save/Adversarial/adversarial4/model_statedict.pt')
+    toRemove = []
+    for weight in model_weights:
+
+        # print ('here is a weight: ', weight)
+        if "realVsPseudo" in weight:
+            toRemove.append(weight)
+
+    for removal in toRemove:
+        print ('removing weight')
+        del model_weights[removal]
+
+    newNames = []
+    vals = []
+    newOrderedDict = collections.OrderedDict()
+    for weight in model_weights:
+        newName = weight[7:]
+        newOrderedDict[newName] = model_weights[weight]
+    adversarial = models.segmentation.fcn_resnet101(pretrained=True, progress=True)
+    adversarial.classifier[4] = nn.Conv2d(512, 2, kernel_size=(1,1), stride=(1,1))
+    adversarial.aux_classifier[4] = nn.Conv2d(256, 2, kernel_size=(1,1), stride=(1,1))
+    adversarial.load_state_dict(newOrderedDict)
+    adversarial.eval()
+
+
+
+    if len(sys.argv) != 2:
+        print ('Use index in arg')
+        quit
+    else:
+        index = sys.argv[1]
 
     # grab a few examples to run through the model
     count = 0
@@ -34,7 +72,7 @@ if (__name__ == '__main__'):
     with os.scandir('../data/deepglobe-dataset-pt/train-labeled-sat') as it:
         for entry in it:
             if not entry.name.startswith('.'):
-                if (count == 10):
+                if (count == index):
                     print (entry.name)
                     examples.append(entry.name)
                 else:
@@ -47,7 +85,9 @@ if (__name__ == '__main__'):
     else:
         device = 'cpu'
 
-    model.to(device)
+    baseline.to(device)
+    adversarial.to(device)
+
 
     #batch = torch.cat(examples)
     #batch = batch.reshape(10, 3, 1024, 1024)
@@ -71,29 +111,15 @@ if (__name__ == '__main__'):
     batch = batch.reshape(NUM_EXAMPLES, 3, 1024, 1024)
     print ('about to run batch')
     
-    output = model(batch)['out'].cpu()
-    output = torch.argmax(output, dim=1) * 255
-    print ('torch.sum(output): ', torch.sum(output))
-    #print ('output: ', output)
-    plt.imsave('../save/Baseline/baseline1/example_outputs/example_output_' + examples[0] + '.png', output[0])
+    with torch.no_grad():
+        baseline_output = baseline(batch)['out'].cpu()
+        baseline_output = torch.argmax(baseline_output, dim=1) * 255
+        adversarial_output = adversarial(batch)['out'].cpu()
+        adversarial_output = torch.argmax(adversarial_output, dim=1) * 255
+    plt.imsave('../save/Baseline/baseline1/example_outputs/baseline_example_output_' + examples[0] + '.png', baseline_output[0])
+    plt.imsave('../save/Adversarial/adversarial4/example_outputs/adversarial_example_output_' + examples[0] + '.png', adversarial_output[0])
+
     print ('saved via plt')
 
-
-
-
-
-    '''
-    # output = output.numpy()
-    print ('output.s: ', output.size)
-    # im = Image.fromarray(output)
-    #print ('output.shape: ', output.shape)
-    #print ('output: ', output)
-    # im.save('..save/Baseline/baseline0/example_outputs/example_output_' + examples[0])
-    predictions = torch.argmax(output, dim=1)
-    for i in range(NUM_EXAMPLES):
-        cur_example = predictions[i]
-        print (cur_example)
-        saveOutputAsImage(cur_example, '..save/Baseline/baseline0/example_outputs/example_output_' + examples[i] + '.jpg')
-    '''
 
 
